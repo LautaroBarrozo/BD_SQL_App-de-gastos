84008a92ed7748ee59344e716cf47cfd7c743556
LautaroBarrozo <lautaro-barrozo@outlook.com>
2023-06-12 12:12:35 -0300

    app de gastos finalizada

--- src/dbConnect.ts
+++ src/dbConnect.ts
@@ -1,5 +1,14 @@
 import mysql, {Connection} from "mysql"
 
+// tabla a crear en la base de datos
+
+// CREATE TABLE spences (
+// 	 id int NOT NULL AUTO_INCREMENT,
+// 	 user_name varchar(255) NOT NULL,
+// 	 spence_name varchar(255) NOT NULL,
+// 	 spence_value int NOT NULL,
+// 	 PRIMARY KEY(id)
+// )
 
 // poner url de base de datos
 const DATABASE_URL = ''
